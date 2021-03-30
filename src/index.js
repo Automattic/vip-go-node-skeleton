6c3d1059db4028db33e66bc089724cd8706690ad
Ahmed <elazzabi.ahmed@gmail.com>
2021-03-30 16:13:14 +0100

    Add ; to end of statements
    Co-authored-by: Brandon Skinner <brandon.skinner@automattic.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get( '/cache-healthcheck', function( req, res ) {
 	res.sendStatus( 200 );
-} )
+} );
 
 app.get( '/', function( req, res ) {
 	res.send( 'Howdy!' );
-} )
+} );
 
 app.listen( PORT, function() {
 	console.log( 'App is listening on port:', PORT );
-} )
+} );
