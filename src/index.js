c3ede6396ac0e1f631330f88c8a4a5c7071a59a3
Henrique Mouta <henrique@mowta.pt>
2021-10-22 17:00:14 +0100

    Update example to use Node's native HTTP module

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
-var express = require( 'express' );
-var app = express();
+const http = require( 'http' );
+
 const PORT = process.env.PORT || 3000;
 
-// Used by the monitoring system on VIP to verify the health of the app
-// Should return 200 when the app is healthy
-// https://docs.wpvip.com/technical-references/vip-platform/node-js/
-app.get( '/cache-healthcheck', function( req, res ) {
-	res.sendStatus( 200 );
+const app = http.createServer( ( req, res ) => {
+	let [ requestPath, queryParams ] = req.url.split( '?' );
+
+	// Remove trailing slash from the request path
+	requestPath = requestPath.length > 1 ? requestPath.replace( /\/+$/, '' ) : requestPath;
+
+	if ( 'GET' !== req.method ) {
+		res.writeHead(404 );
+		res.end( );
+		return;
+	}
+
+	if ( '/' === requestPath ) {
+		res.writeHead( 200 );
+		res.end( "Howdy!" );
+		return;
+	}
+
+	// Used by the monitoring system on VIP to verify the health of the app
+	// Should return 200 when the app is healthy
+	// https://docs.wpvip.com/technical-references/vip-platform/node-js/
+	if ( '/cache-healthcheck' === requestPath ) {
+		res.writeHead( 200 );
+		res.end( );
+		return;
+	}
+
+	res.writeHead( 404 );
+	res.end( );
 } );
 
-app.get( '/', function( req, res ) {
-	res.send( 'Howdy!' );
-} );
 
-app.listen( PORT, function() {
+app.on( 'listening', () => {
 	console.log( 'App is listening on port:', PORT );
 } );
+
+app.listen( PORT );
