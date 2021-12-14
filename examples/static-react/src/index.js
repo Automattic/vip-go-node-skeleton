9e644d78ad1db328ef48ed1dcf540aaf6d275b3c
Roger Theriault <roger.theriault@automattic.com>
2021-12-14 16:19:53 -0500

    remove trailing slash from default localhost URL to accommodate a port suffix
    Co-authored-by: Chris Zarate <chris.zarate@automattic.com>

--- examples/static-react/src/index.js
+++ examples/static-react/src/index.js
@@ -8,7 +8,7 @@ const path = require( 'path' );
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const BASEURL = process.env.BASEURL || 'http://localhost/';
+const BASEURL = process.env.BASEURL || 'http://localhost';
 
 /**
  * Handle healthcheck requests
