9594a70cde4a912ba5057a60ba9649262e4f0011
chriszarate <chris.zarate@automattic.com>
2023-11-30 13:33:04 -0500

    Code and documentation updates

--- examples/cache-personalization/index.js
+++ examples/cache-personalization/index.js
@@ -6,55 +6,80 @@
  */
 
 const express = require( 'express' );
-const cookieParser = require("cookie-parser");
+const cookieParser = require( 'cookie-parser' );
 
 const PORT = process.env.PORT || 3000;
 const BASEURL = process.env.BASEURL || 'http://localhost';
 
-// The cookie name must be "vip-go-seg"
+// In order to be recognized by WPVIP's page cache, segments must be stored in a
+// cookie named "vip-go-seg".
 const COOKIE_NAME = 'vip-go-seg';
 
 const app = express();
 
-app.use(cookieParser());
+// "cookie-parser" provides easier access to cookies sent with the request.
+app.use( cookieParser() );
 
 /**
- * Handle healthcheck requests
- * 
- * This is a requirement for any application running
- * in the VIP environment:
- * - must handle url "/cache-healthcheck?" with trailing question mark
- * - must respond with 200 status
- * - must only send a short response eg "ok"
- * - must respond immediately, without delay
- * - must prioritize this above other routes
- * - should not execute significant code before, during, or after
- * - must end the request/response (send the response, which fulfills the request)
- * 
- * Test: curl -v "https://example.com/cache-healthcheck?"
+ * WPVIP health check
+ *
+ * https://docs.wpvip.com/technical-references/node-js/health-checks/
  */
 app.get( '/cache-healthcheck', function (req, res) {
 	res.status( 200 ).send( 'Ok' );
 } );
 
+// Homepage route
 app.get( '/', function ( req, res ) {
+	// Get the currently selected theme from the cookie (or use the default).
 	const theme = getSafeThemeValue( req.cookies[ COOKIE_NAME ] );
+
+	// Assemble the response, respecting the current theme. Provide a button that
+	// allows the user to update the theme via an API endpoint (implemented below).
 	const html = `
 <!doctype html>
-<html>
+<html data-theme="${ theme }">
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>WPVIP Cache Personalization API Example</title>
 		<style>
+			/* Server-side rendered theme */
+			:root {
+					--background-color: #F9F9F9;
+					--text-color: #000000;
+					--theme-name: 'light';
+			}
+
+			html[data-theme='dark'] {
+					--background-color: #000000;
+					--text-color: #F9F9F9;
+					--theme-name: 'dark';
+			}
+
 			body {
-				background-color: ${ 'dark' === theme ? '#000000' : '#F9F9F9' };
-				color: ${ 'dark' === theme ? '#F9F9F9' : '#000000' };
+				background-color: var(--background-color);
+				color: var(--text-color);
+			}
+
+			span#theme::after {
+				content: var(--theme-name);
 			}
 		</style>
+		<script>
+			/* Client-side theme toggle */
+			function toggleTheme() {
+				fetch( '/api/toggle-theme', { method: 'post' } )
+					.then( response => response.json() )
+					.then( payload => document.documentElement.dataset.theme = payload.theme );
+			}
+		</script>
 	</head>
 	<body>
 		<h1>WPVIP Cache Personalization API Example</h1>
-		<p>Current theme: ${ theme }</p>
-		<button onClick="fetch( '/api/toggle-theme', { method: 'post' } ).then( () => location.reload() );">
+		<p>Current theme: <span id="theme"></span></p>
+
+		<!-- Update the theme and rerender -->
+		<button onClick="toggleTheme();">
 			Toggle theme
 		</button>
 	</body>
@@ -66,25 +91,34 @@ app.get( '/', function ( req, res ) {
 	res.status( 200 ).send( html );
 } );
 
+// API endpoint route
 app.post( '/api/toggle-theme', function ( req, res ) {
+	// Get the currently selected theme from the cookie (or use the default).
 	const currentTheme = getSafeThemeValue( req.cookies[ COOKIE_NAME ] );
+
+	// Toggle the theme.
 	const newTheme = 'dark' === currentTheme ? 'light' : 'dark';
 
-	const options = {
+	const cookieOptions = {
         httpOnly: true,
         maxAge: 60 * 60 * 24 * 365, // one year in seconds
 				sameSite: 'strict',
     };
 
-	res.cookie( COOKIE_NAME, newTheme, options );
-	res.status( 200 ).send( newTheme );
+	// Set a cookie that will be sent with subsequent requests.
+	res.cookie( COOKIE_NAME, newTheme, cookieOptions );
+
+	// The application will use this response to rerender the page with the new theme.
+	res.status( 200 ).json( { theme: newTheme } );
 } );
 
+// This function ensures that users cannot inject an unsupported theme value.
 function getSafeThemeValue( themeInput = '' ) {
 	if ( 'dark' === themeInput.toLowerCase() ) {
 		return 'dark';
 	}
 
+	// Default value for requests without a cookie or malformed cookies.
 	return 'light';
 }
 
